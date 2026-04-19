d5b41ea91c9477f0512bf7c217411cb81d85803b
J0key <shyraathaya@mail.ugm.ac.id>
2026-04-20 00:20:39 +0700

    fix: gitignore

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const profileRoutes = require("./routes/profile")
 const listingRoutes = require("./routes/listings")
 const bookingRoutes = require("./routes/booking")
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors())
@@ -28,4 +30,5 @@ mongoose
     .then(() => console.log("Mongodb Connected"))
     .catch((err) => console.error(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
